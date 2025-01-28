f11fad08313b3374bcc659013734ad24c4f349ba
Pang Ai Jie Jennie <s10268150@connect.np.edu.sg>
2025-01-29 00:26:02 +0800

    Done with my parts for basic features! jiayou!

--- Flight.cs
+++ Flight.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace Assignment
 {
-    abstract class Flight
+    abstract class Flight : IComparable<Flight>
     {
         public string FlightNumber { get; set; }
         public string Origin { get; set; }
         public string Destination { get; set; }
         public DateTime ExpectedTime { get; set; }
-        public string Status { get; set; } = "Unassigned";
+        public string Status { get; set; } = "Scheduled";
         public Flight() { }
         public Flight(string f, string o, string d, DateTime e)
         {
@@ -36,6 +36,14 @@ namespace Assignment
                 return 0;
             }
         }
+
+        public int CompareTo(Flight other)
+        {
+            if (ExpectedTime < other.ExpectedTime) return -1;
+            else if (other.ExpectedTime == ExpectedTime) return 0;
+            else return 1;
+        }
+
         public override string ToString()
         {
             return "Flight Number: " + FlightNumber + 
--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿//Ho Zhen Yi S10267291 : Q1, Q4, Q7, Q8
 //Pang Ai Jie Jennie S10268150 : Q2, Q3, Q5, Q6, Q9
 
+//Dictionary list: (so we dont keep scrolling through the code LOL)
+//Dictionary<string, string> FlightNumToGate
+//Dictionary<string, string> airlinesDict
+//Dictionary<string, BoardingGate> boardinggateDict
+//Dictionary<string, Flight> flightsDict
+//Dictionary<string, string> FlightAndSpecialCodeDict
+
 using System;
+using System.ComponentModel;
 using Assignment;
 
 //Q1 completed - Ho Zhen Yi S10267291
@@ -32,7 +40,6 @@ for (int i = 1; i < boardinggateLines.Length; i++)
 Dictionary<string, Flight> flightsDict = new Dictionary<string, Flight>();
 Dictionary<string, string> FlightAndSpecialCodeDict = new Dictionary<string, string>();
 string[] flightLines = File.ReadAllLines("flights.csv");
-
 for (int i = 1; i < flightLines.Length; i++)
 {
     string[] data = flightLines[i].Trim().Split(',');
@@ -59,13 +66,13 @@ for (int i = 1; i < flightLines.Length; i++)
         f = new NORMFlight(flightNumber, origin, destination, expectedDate);
     }
     flightsDict.Add(f.FlightNumber, f);
-    if (specialCode != null)
+    if (specialCode == "DDJB" || specialCode == "CFFT" || specialCode == "LWTT")
     {
-        FlightAndSpecialCodeDict.Add(f.FlightNumber, specialCode);
+        FlightAndSpecialCodeDict[f.FlightNumber] = specialCode;
     }
     else
     {
-        FlightAndSpecialCodeDict.Add(f.FlightNumber, "None");
+        FlightAndSpecialCodeDict[f.FlightNumber] = "None";
     }
 }
 
@@ -94,19 +101,63 @@ void ListBoardingGates(Dictionary<string, BoardingGate> boardinggateDict)
     }
 }
 
-//Q5 halfway - Pang Ai Jie Jennie S10268150
+//Q5 completed - Pang Ai Jie Jennie S10268150
+Dictionary<string, string> FlightNumToGate = new Dictionary<string, string>();
+//create dictionary to store flight number and boarding gate name
+foreach (KeyValuePair<string, Flight> Kvp in flightsDict)
+{
+    FlightNumToGate.Add(Kvp.Value.FlightNumber, "Unassigned");
+}
 void AssignBoardingGateToFlight(Dictionary<string, Flight> flightsDict, Dictionary<string, BoardingGate> boardinggateDict, Dictionary<string, string> FlightAndSpecialCodeDict)
 {
     while (true)
     {
-        Console.Write("Enter Flight Number:");
-        string flightNum = Console.ReadLine().ToUpper();
-        Console.Write("Enter Boarding Gate Name:");
+        Console.WriteLine("Enter Flight Number:");
+        string flightNum = Console.ReadLine().ToUpper().Replace(" ", "");
+        Console.WriteLine("Enter Boarding Gate Name:");
         string boardingGateName = Console.ReadLine().ToUpper();
-
-        //display basic info of flight
-        if (flightsDict.ContainsKey(flightNum) && boardinggateDict.ContainsKey(boardingGateName))
+        bool flightExists = false;
+        foreach (KeyValuePair<string, Flight> kvp in flightsDict)
         {
+            if (kvp.Key.Replace(" ", "") == flightNum)
+            {
+                flightNum = kvp.Key;
+                flightExists = true;
+                break;
+            }
+        }
+        if (flightExists && boardinggateDict.ContainsKey(boardingGateName))
+        {
+            //assign gate to flight
+            foreach (KeyValuePair<string, string> KVp in FlightNumToGate)
+            {
+                if (KVp.Key == flightNum)
+                {
+                    if (boardinggateDict[boardingGateName].SupportsDDJB == true && FlightAndSpecialCodeDict[flightNum] == "DDJB")
+                    {
+                        FlightNumToGate[KVp.Key] = boardingGateName;
+                    }
+                    else if (boardinggateDict[boardingGateName].SupportsCFFT == true && FlightAndSpecialCodeDict[flightNum] == "CFFT")
+                    {
+                        FlightNumToGate[KVp.Key] = boardingGateName;
+                    }
+                    else if (boardinggateDict[boardingGateName].SupportsLWTT == true && FlightAndSpecialCodeDict[flightNum] == "LWTT")
+                    {
+                        FlightNumToGate[KVp.Key] = boardingGateName;
+                    }
+                    else if (FlightAndSpecialCodeDict[flightNum] == "None")
+                    {
+                        FlightNumToGate[KVp.Key] = boardingGateName;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Boarding Gate does not support the flight's special request code. Please try again.");
+                        break;
+                    }
+                }
+            }
+
+            //display basic info of flight
             Flight f = flightsDict[flightNum];
             BoardingGate b = boardinggateDict[boardingGateName];
             b.Flight = f;
@@ -126,12 +177,14 @@ void AssignBoardingGateToFlight(Dictionary<string, Flight> flightsDict, Dictiona
                     break;
                 }
             }
-            Console.Write("Would you like to update the status of the flight? (Y/N)");
+
+            //update status of flight
+            Console.WriteLine("Would you like to update the status of the flight? (Y/N)");
             string response = Console.ReadLine().ToUpper();
             if (response == "Y")
             {
                 Console.WriteLine("1. Delayed" + "\n2. Boarding" + "\n3. On Time");
-                Console.Write("Please select the new status of the flight:");
+                Console.WriteLine("Please select the new status of the flight:");
                 string newStatusOption = Console.ReadLine();
                 if (newStatusOption == "1")
                 {
@@ -150,7 +203,6 @@ void AssignBoardingGateToFlight(Dictionary<string, Flight> flightsDict, Dictiona
                     Console.WriteLine("Invalid option. Please try again.");
                     continue;
                 }
-                Console.WriteLine($"Flight {f.FlightNumber} has been assigned to Boarding Gate {b.GateName}!");
             }
             else if (response == "N")
             {
@@ -161,6 +213,8 @@ void AssignBoardingGateToFlight(Dictionary<string, Flight> flightsDict, Dictiona
                 Console.WriteLine("Invalid option. Please try again.");
                 continue;
             }
+            Console.WriteLine($"Flight {f.FlightNumber} has been assigned to Boarding Gate {b.GateName}!");
+            break;
         }
         else
         {
@@ -170,18 +224,129 @@ void AssignBoardingGateToFlight(Dictionary<string, Flight> flightsDict, Dictiona
     }
 }
 
-//Q6
+//Q6 - completed Pang Ai Jie Jennie S10268150
+void CreateFlight(Dictionary<string, Flight> flightsDict)
+{
+    while (true)
+    {
+        Console.WriteLine("Enter Flight Number: ");
+        string flightNumber = Console.ReadLine();
+        Console.WriteLine("Enter Origin: ");
+        string origin = Console.ReadLine();
+        Console.WriteLine("Enter Destination: ");
+        string destination = Console.ReadLine();
+        Console.WriteLine("Enter Expected Departure/Arrival Time (dd/mm/yyyy hh:mm): ");
+        DateTime expectedTime = Convert.ToDateTime(Console.ReadLine());
+        Console.WriteLine("Enter Special Request Code (CFFT/DDJB/LWTT/None): ");
+        string specialCode = Console.ReadLine();
 
-//Q7
+        //create flight object & add to dictionary
+        Flight fl;
+        if (specialCode == "CFFT")
+        {
+            fl = new CFFTFlight(flightNumber, origin, destination, expectedTime);
+        }
+        else if (specialCode == "DDJB")
+        {
+            fl = new DDJBFlight(flightNumber, origin, destination, expectedTime);
+        }
+        else if (specialCode == "LWTT")
+        {
+            fl = new LWTTFlight(flightNumber, origin, destination, expectedTime);
+        }
+        else
+        {
+            fl = new NORMFlight(flightNumber, origin, destination, expectedTime);
+        }
+        flightsDict.Add(fl.FlightNumber, fl);
 
-//Q8
+        //append new flight info to flights.csv
+        if (specialCode == "None")
+        {
+            string[] lines = { $"{flightNumber},{origin},{destination},{expectedTime},{null}" };
+            File.WriteAllLines("flights.csv", lines);
+        }
+        else
+        {
+            string[] lines = { $"{flightNumber},{origin},{destination},{expectedTime},{specialCode}" };
+            File.WriteAllLines("flights.csv", lines);
+        }
+        Console.WriteLine($"Flight {fl.FlightNumber} has been added!");
 
-//Q9
+        //prompt user to add another flight
+        Console.WriteLine("Would you like to add another flight? (Y/N)");
+        string response = Console.ReadLine().ToUpper();
+        if (response == "Y")
+        {
+            continue;
+        }
+        else if (response == "N")
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Invalid option. Please try again.");
+            continue;
+        }
+    }
+}
+
+//List of Airlines for Changi Airport Terminal 5
+void ListOfAirlines(Dictionary<string, string> airlinesDict)
+{
+    Console.WriteLine("=============================================" +
+        "\nList of Airlines for Changi Airport Terminal 5" +
+        "\n=============================================");
+    Console.WriteLine($"{"Airline Code",-16}{"Airline Name"}");
+    foreach (KeyValuePair<string, string> kvp in airlinesDict)
+    {
+        Console.WriteLine($"{kvp.Value,-16}{kvp.Key}");
+    }
+}
+
+//Q7 - Ho Zhen Yi S10267291
+void DisplayAirlineFlights(Dictionary<string, string> airlinesDict, Dictionary<string, Flight> flightsDict)
+{
+    Console.WriteLine("Enter Airline Name: ");
+    string airlineName = Console.ReadLine();
+    Console.WriteLine($"{"============================================="}" +
+    $"{"\nList of Flights for {}"}" + //need to get airline name
+    $"{"\n============================================="}");
+    Console.WriteLine($"{"Flight Number", -16}{"Airline Name", -23}{"Origin", -23}{"Destination", -23}{"Expected Departure/Arrival Time"}");
+}
+
+//Q8 - Ho Zhen Yi S10267291
+void ModifyFlightDetailed(Dictionary<string, Flight> flightsDict)
+{
+    ListOfAirlines(airlinesDict);
+    DisplayAirlineFlights(airlinesDict, flightsDict);
+    Console.WriteLine("");
+}
+
+//Q9 - completed Pang Ai Jie Jennie S10268150
+void DisplayFlightSchedule(Dictionary<string, Flight> flightsDict, Dictionary<string, string> airlinesDict, Dictionary<string, string> FlightNumToGate)
+{
+    var sortedFlights = flightsDict.Values.ToList();
+    sortedFlights.Sort();
+    Console.WriteLine($"{"Flight Number",-16}{"Airline Name",-23}{"Origin",-23}{"Destination",-23}{"Expected Departure/Arrival Time",-36}{"Status",-16}{"Boarding Gate"}");
+    foreach (KeyValuePair<string, Flight> kvp in flightsDict)
+    {
+        foreach (KeyValuePair<string, string> keyvaluepair in airlinesDict)
+        {
+            if (kvp.Value.FlightNumber.Contains(keyvaluepair.Value))
+            {
+                string airlineName = keyvaluepair.Key;
+                Console.WriteLine($"{kvp.Value.FlightNumber,-16}{airlineName,-23}{kvp.Value.Origin,-23}{kvp.Value.Destination,-23}{kvp.Value.ExpectedTime, -36}{kvp.Value.Status, -16}{FlightNumToGate[kvp.Value.FlightNumber]}");
+            }
+        }
+    }
+}
 
 //main loop
 while (true)
 {
-    Console.WriteLine("=============================================" +
+    Console.WriteLine("\n\n\n\n\n=============================================" +
         "\nWelcome to Changi Airport Terminal 5" +
         "\n=============================================" +
         "\n1. List All Flights" +
@@ -192,8 +357,7 @@ while (true)
         "\n6. Modify Flight Details" +
         "\n7. Display Flight Schedule" +
         "\n0. Exit");
-    Console.WriteLine(" ");
-    Console.WriteLine("Please select your option:");
+    Console.WriteLine("\n\nPlease select your option:");
     int option = Convert.ToInt32(Console.ReadLine());
 
     if (option == 1)
@@ -214,26 +378,34 @@ while (true)
     }
     else if (option == 3)
     {
+        Console.WriteLine("=============================================" +
+                         "\nAssign a Boarding Gate to a Flight" +
+                         "\n=============================================");
         AssignBoardingGateToFlight(flightsDict, boardinggateDict, FlightAndSpecialCodeDict);
     }
     else if (option == 4)
     {
-
+        CreateFlight(flightsDict);
     }
     else if (option == 5)
     {
-
+        ListOfAirlines(airlinesDict);
+        DisplayAirlineFlights(airlinesDict, flightsDict);
     }
     else if (option == 6)
     {
-
+        ModifyFlightDetailed(flightsDict);
     }
     else if (option == 7)
     {
-
+        Console.WriteLine("=============================================" +
+            "\nFlight Schedule for Changi Airport Terminal 5" +
+            "\n=============================================");
+        DisplayFlightSchedule(flightsDict, airlinesDict, FlightNumToGate);
     }
     else if (option == 0)
     {
+        Console.WriteLine("Goodbye!");
         break;
     }
     else
